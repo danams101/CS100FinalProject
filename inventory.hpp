61c5efc91bdfe6b9c5be7964b3992a67253b8968
github-username <ffan005@ucr.edu>
2021-02-20 10:33:11 -0800

    wrote code according to diagram, nothing fancy

---
+++ inventory.hpp
@@ -0,0 +1,53 @@
+#ifndef _INVENTORY_HPP_
+#define _INVENTORY_HPP_
+
+#include vector
+
+class Inventory{
+private:
+	int numWood = 0;
+	int numMeat = 0;
+	int numMetal = 0;
+	int numSteels = 0;
+Public:
+	void SetWood(){
+	numWood = 0;
+}
+	void SetWood(int num){
+	numWood = num;
+}
+        void SetMeat(){
+        numMeat = 0;
+}
+        void SetMeat(int num){
+        numMeat = num;
+}
+        void SetMetal(){
+        numMetal = 0;
+}
+        void SetMetal(int num){
+        numMetal = num;
+}
+        void SetSteels(){
+        numSteels = 0;
+}
+        void SetSteels(int num){
+        numSteels = num;
+}
+
+	int getnumWood(){
+	return numWood;	
+}
+	int getnumMeat(){
+	return numMeat;
+}
+        int getnumMetal(){
+        return numMetal;
+}
+        int getnumMetal(){
+        return numMetal;
+}
+	
+};
+
+#endif
