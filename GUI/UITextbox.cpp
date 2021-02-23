336ddc4f48d3a690957fd826faebc90b473837bf
Dana Savin <danams101@yahoo.com>
2021-02-23 12:54:18 -0800

    Added comments and descriptions.

--- GUI/UITextbox.cpp
+++ GUI/UITextbox.cpp
@@ -1,6 +1,13 @@
 #include "UITextbox.hpp"
 
+// Sets text size, the color of text, default selection (if the user can type in it),
+// and the charater limit.
 UITextbox::UITextbox(int textSize, sf::Color color, bool isSel, int charLimit){
+    /*
+    Initializes everything.
+    A selected textbox is indicated by an underscore appearing.
+    Note: A textbox is selected by pressing TAB.
+    */
     textbox.setCharacterSize(textSize);
     textbox.setFillColor(color);
     isSelected = isSel;  
@@ -14,19 +21,27 @@ UITextbox::UITextbox(int textSize, sf::Color color, bool isSel, int charLimit){
     }
 }
 
+//Sets text color.
 void UITextbox::setTextColor(sf::Color color){
     textbox.setFillColor(color);
 }
 
+//Sets the font, needs to be set when called.
 void UITextbox::setFont(sf::Font& font){
     textbox.setFont(font);
 }
 
+//Sets position, needs to be set when called.
 void UITextbox::setPosition(sf::Vector2f pos){
     textbox.setPosition(pos);
 }
 
+//Sets whether a textbox is selected.
 void UITextbox::setSelection(bool ToF){
+    /*
+    If the textbox is not selected/unselected, the string is saved.
+    */
+    
     isSelected = ToF;
     if(!isSelected){
         std::string s = "";
@@ -37,11 +52,21 @@ void UITextbox::setSelection(bool ToF){
     }
 }
 
+//Returns string that user typed.
 std::string UITextbox::getText(){
     return ss.str();
 }
 
+//Controls behavior of textbox when the user types in it.
 void UITextbox::typedOn(sf::Event textEntered){
+    /*
+    Activates if the textbox is selected.
+
+    Takes in an SFML event (sf::Event::TextEntered), uses this event to grab the 
+    characters the user entered in unicode. 
+    Checks if the charaters are valid. ( <128 ), calls private InputLogic function.
+    Checks if character limit is reached, if it is only allows delete behavior (backspace()).
+    */
     if(isSelected){
         int charTypedValue = textEntered.text.unicode;
 
@@ -57,11 +82,22 @@ void UITextbox::typedOn(sf::Event textEntered){
     }
 }
 
+//Draws object to window.
 void UITextbox::drawTo(sf::RenderWindow& window){
     window.draw(textbox);
 }
 
+//Private functions
+
+// Handles input logic for typedOn function.
 void UITextbox::InputLogic(int charTypedVal){
+    /*
+    Checks for special key inputs, if no special keys, it outputs the char to the
+    string stream ss.
+
+    If the typed key is the delete key, call backspace function.
+    */
+    
     if(charTypedVal != DELETE_KEY && charTypedVal!= TAB_KEY && charTypedVal != DELETE_KEY){
         ss << static_cast<char>(charTypedVal);
     }
@@ -73,7 +109,16 @@ void UITextbox::InputLogic(int charTypedVal){
     textbox.setString(ss.str() + "_");
 }
 
+//Deletes the last character of the string.
 void UITextbox::backspace(){
+    /*
+    Simple delete function.
+    Copys old string to new string with one less char,
+    clears string stream, outputs new string to ss.
+
+    Sets the textbox's string to the new ss.str().
+    */
+
     std::string origS = ss.str();
     std::string newS = "";
 
--- GUI/UITextbox.hpp
+++ GUI/UITextbox.hpp
@@ -7,17 +7,22 @@
 
 class UITextbox : public UIObject {
     public:
-
+        //Constructor
         UITextbox(int textSize, sf::Color color, bool isSelected, int charLimit);
 
+        //Setters
         void setTextColor(sf::Color color);
         void setFont(sf::Font& font);
         void setPosition(sf::Vector2f pos);
-
         void setSelection(bool ToF);
+        
+        //Getters
         std::string getText();
+
+        //Functions
         void typedOn(sf::Event textEntered);
 
+        //Virtual function from UIObject class
         virtual void drawTo(sf::RenderWindow& window);
 
 
@@ -26,16 +31,16 @@ class UITextbox : public UIObject {
         std::ostringstream ss;
         bool isSelected;
 
-        //We need an input character limit
+        //Input character limit
         int limit;
 
-        // we get input text in unicode from sfml
+        // Declared special keys using unicode for special characters.
         int DELETE_KEY = 8;
         int TAB_KEY = 9;
         int ENTER_KEY = 13;
-        void InputLogic(int charTypedVal);
 
-        //delete the last character function
+        //Private functions
+        void InputLogic(int charTypedVal);
         void backspace();
 
 };
