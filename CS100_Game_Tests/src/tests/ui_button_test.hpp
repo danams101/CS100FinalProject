409c5faa34d9ece1917fb4eb3b20772ec579d6f1
github-username <ffan005@ucr.edu>
2021-03-10 10:34:47 -0800

    added UIObject tests

--- CS100_Game_Tests/src/tests/ui_button_test.hpp
+++ CS100_Game_Tests/src/tests/ui_button_test.hpp
@@ -13,23 +13,16 @@ class UIButtonTest : public TestSuite {
     private:
 
     public:
-        UIButtonTest(GlobalData* global_data) : TestSuite(global_data) {
-            this->name = "UIButtonTest";
+        UIObjectTest(GlobalData* global_data) : TestSuite(global_data) {
+            this->name = "UIObjectTest";
         }
-		virtual ~UIButtonTest() {
+		virtual ~UIObjectTest() {
 		}
 
 		void test() {
-		    expectedTests(4);
-            std::string title;
+		expectedTests(8);
+        	std::string title;
 
-            // SubEvaluateNonZero
-            title = "SubEvaluateNonZero";
-            int a = 5;
-            int b = 2;
-            this->expect_eq(a - b, 3, title);
-
-            // XPosAt1
             title = "XPosAt1";
             float x = 1;
             float y = 1;
@@ -39,15 +32,28 @@ class UIButtonTest : public TestSuite {
             UIButton* temp1 = new UIButton(x, y, width, height, colors);
             this->expect_eq(temp1->getX(), 1, title);
 
-            // CenteredFalse
-            title = "CenteredFalse";
-            this->expect_eq(temp1->isCentered(), false, title);
+            title = "YPosAt1";
+            this->expect_eq(temp1->getY(), 1, title);
 
-            // EmptyText
-            title = "EmptyText";
-            this->expect_eq(temp1->getText(), "", title);
 
+            title = "WidthAt1";
+            this->expect_eq(temp1->getWidth(),1,title);
+
+            title = "HeightAt1";
+            this->expect_eq(temp1->getHeight(),1,title);
+
+            title = "SetPosition";
+            temp1->setPosition(1,1);
+            this->expect_eq(temp1->getX(),1,title);
+            this->expect_eq(temp1->getY(),1,title);
+
+            title = "SetSize";
+            temp1->setSize(10,10);
+            this->expect_eq(temp1->getWidth(),10,title);
+            this->expect_eq(temp1->getHeight(),10,title);
             ranTests();
+
+            delete temp1;
 		}
 };
 
