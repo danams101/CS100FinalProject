4ef5fdd3fa17d236c3d71f1fc3d4c69dfa68fb37
github-username <ffan005@ucr.edu>
2021-03-10 20:35:04 -0800

    updated launcher

--- CS100_Game_Tests/src/launcher.cpp
+++ CS100_Game_Tests/src/launcher.cpp
@@ -8,6 +8,14 @@
 // Tests
 #include "tests/ui_button_test.hpp"
 #include "tests/ui_button_test2.hpp"
+#include "tests/ui_statbar_test.hpp"
+#include "tests/ui_buttontimer_test.hpp"
+#include "tests/ui_list_test.hpp"
+#include "tests/ui_displaytext_test.hpp"
+#include "tests/ui_textbox_test.hpp"
+#include "tests/inventory_test.hpp"
+#include "tests/campStatus_test.hpp"
+#include "tests/state_test.hpp"
 
 using namespace std;
 
@@ -42,6 +50,13 @@ void initTests(TestEngine* bing_tests) {
     // UIButtonTest
     bing_tests->addTestSuite(new UIObjectTest(bing_tests->getGlobalData()));
     bing_tests->addTestSuite(new UIButtonTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new UIStatBarTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new UIButtonTimerTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new UIListTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new UIDisplayTextTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new UITextboxTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new InventoryTest(bing_tests->getGlobalData()));
+    bing_tests->addTestSuite(new CampStatusTest(bing_tests->getGlobalData()));
 
     console->print("[==========] ", 1);
     console->print("Finalized Initializing Test Suites\n", 0);
